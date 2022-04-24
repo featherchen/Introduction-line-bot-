5445e84fda4a948127112596b758e92560d74b8a
featherchen <garychen0975321741@gmail.com>
2022-04-24 18:02:58 +0800

    test format

--- introlinebot/views.py
+++ introlinebot/views.py
@@ -7,7 +7,8 @@ from linebot import LineBotApi, WebhookParser
 from linebot.exceptions import InvalidSignatureError, LineBotApiError
 from linebot.models import MessageEvent, TextSendMessage
 
-from reply import academic_intro
+from .reply import academic_intro
+
 line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
 parser = WebhookParser(settings.LINE_CHANNEL_SECRET)
     
