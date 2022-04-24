a09f29be3d1f6aa52f0af03712b81b6e448bc79d
featherchen <garychen0975321741@gmail.com>
2022-04-24 18:49:52 +0800

    test format

--- introlinebot/reply/academic_intro.py
+++ introlinebot/reply/academic_intro.py
@@ -1,1 +1,21 @@
-academic_intro = "我是臺灣大學三年級的學生，主修是電機系跟資工系，同時也是Apache基金會底下的committer。\n\n我的興趣廣泛，對於軟體硬體都有涉略，也有網站架設跟open source的相關經驗。\n\n在台大修過的課程包含：網路服務程式設計、計算機安全、資料結構與演算法、演算法設計與分析、系統程式設計、網路與多媒體實驗、機器學習......\n\n目前的興趣主要在於軟體工程、資訊安全等領域。"
+academic_intro = """
+dsafsdfsdafsdaffffffffdsaadssssssssssssssssssssssssssssss
+
+asfdsafsadfsdflkl;kl;klks;adlfklsadkf;lsadkfl;sadkflsdkfl;kasd;l
+
+asfdsafsadfsdflkl;kl;klks;adlfklsadkf;lsadkfl;sadkflsdkfl;kasd;l
+
+dsfasdfsadfsdfsafasdddddddddddddddddddddddddddddddddddddddddddddddddddddddd
+"""
+
+
+
+# academic_intro = """
+# 我是臺灣大學三年級的學生，主修是電機系跟資工系，同時也是Apache基金會底下的committer。
+
+# 我的興趣廣泛，對於軟體硬體都有涉略，也有網站架設跟open source的相關經驗。
+
+# 在台大修過的課程包含：網路服務程式設計、計算機安全、資料結構與演算法、演算法設計與分析、系統程式設計、網路與多媒體實驗、機器學習......
+
+# 目前的興趣主要在於軟體工程、資訊安全等領域。
+# """
