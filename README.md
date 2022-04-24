995c77ba0229e2d582c5bb8cb6012ef69558b534
featherchen <garychen0975321741@gmail.com>
2022-04-25 00:15:03 +0800

    fix sentence

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Self-introduction-line-bot
 
 A self introduction of Chen Liang-Chun
+
+## Result
+
+## Resourse
+
+https://www.learncodewithmike.com/2020/06/python-line-bot.html
+https://www.learncodewithmike.com/2020/07/python-line-bot-deploy-to-heroku.html
+https://stackoverflow.com/questions/71712258/error-could-not-build-wheels-for-backports-zoneinfo-which-is-required-to-insta
+https://stackoverflow.com/questions/53209189/typeerror-init-missing-1-required-positional-argument-app-module
--- introlinebot/intro.py
+++ introlinebot/intro.py
@@ -6,7 +6,7 @@ academic_intro = """我是臺灣大學三年級的學生，主修是電機系跟
 
 目前的興趣主要在於軟體工程、資訊安全等領域。"""
 
-open_source_intro = """我從去年3月開始參與Apache Submarine（Top level project in ASF)的開發，這是一個ML的end to end 平台，主要目的是幫助機器學習的科學家可以更好地專注在演算法開發上。
+open_source_intro = """我從去年3月開始參與Apache Submarine（Top level project in ASF)的開發，這是一個ML的end to end 平台，主要目的是幫助從事機器學習的開發的科學家或工程師可以更好地專注在開發工作上，而不須擔心較為繁複的環境設定問題。
 
 Submarine主要的特點在於支援分散式的訓練以及提供Model serving的服務，未來也預計會納入multi-tenant之類的功能。
 
@@ -14,13 +14,13 @@ Submarine主要的特點在於支援分散式的訓練以及提供Model serving�
 
 詳細可以參考：https://github.com/apache/submarine"""
 
-side_project_intro = """EE+網站是一個用來聯繫電機系系友跟在學生的平台，以打造更堅強的NTUEE互助圈為宗旨，主要功能包括：留學配對、內推資訊整理、採訪文章整理等等
+side_project_intro = """NTUEE+網站是一個用來聯繫電機系系友跟在學生的平台，以打造更堅強的NTUEE互助圈為宗旨，主要功能包括：留學配對、內推資訊整理、採訪文章整理等等。
 
-我主要參與網站整體架構規劃以及前端部份的開發與維護。網站預計這學期末會正式上線，詳細可以參考架在heroku上的beta version(久沒連線的話可能會需要等一下): https://eeplus.herokuapp.com/ """
+我主要參與網站整體架構規劃以及前端部份的開發與維護。網站預計這學期末會正式上線，詳細可以參考架在heroku上的beta version(有時候可能會需要等一下): https://eeplus.herokuapp.com/ """
 
-reason_intro = """Line是全球數一數二的即時通訊軟體，在臺灣更是幾乎所有人都有使用，身為軟體工程師，自然是希望自己參與開發的產品能被全世界的人所使用，因此當得知有機會加入Line進行1年的實習時，我便立即投遞履歷了。
+reason_intro = """Line是全球數一數二的即時通訊軟體，在臺灣更是幾乎所有人都有使用，身為軟體工程師，自然會希望自己參與開發的產品能被全世界的人所使用，因此當得知有機會加入Line進行1年的實習時，我便立即投遞履歷了。
 
-這個計畫的Qualifications跟我的背景十分符合，我也有數次使用line提供的sdk的經驗，對於這些工具並不陌生。希望能有機會加入Line跟貢獻我的能力。"""
+這個計畫的Qualifications跟我的背景十分符合，我也有數次使用line提供的sdk的經驗，對於這些工具並不陌生。希望能有機會加入Line學習並貢獻我的能力。"""
 
 tech_intro = """我擁有的技能簡介如下
 Programming： C, C++, Python, Javascript, Node.js, Shell Script, MATLAB
